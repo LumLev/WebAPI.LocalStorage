6f6dafa65b90120c1b2922b682e5e60302e011e9
LumLev <fatlum.latif@outlook.com>
2023-09-24 23:11:53 +0200

    intellisense summaries

---
+++ Readme.md
@@ -0,0 +1,11 @@
+## Local Storage
+
+### Add and inject the service of class named `TheLocalStorage`
+That class contains the methods from https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage
+
+```
+I. SetItem(string key, string value)
+II. GetItem(string key)
+III. RemoveItem(string key)
+IV. Clear()
+```
--- TheLocalStorage.cs
+++ TheLocalStorage.cs
@@ -13,6 +13,11 @@ namespace WebAPI.LocalStorage
                 "import", "./_content/WebAPI.LocalStorage/TheLocalStorage.js").AsTask());
         }
 
+        /// <summary>
+        /// Set a data item in the Local Storage of the user's browser
+        /// </summary>
+        /// <param name="key">The key identifier of the data item</param>
+        /// <param name="value">The value of the data item</param>
         public async ValueTask SetItem(string key, string value)
         {
             var module = await moduleTask.Value;
@@ -20,18 +25,35 @@ namespace WebAPI.LocalStorage
         }
 
 
+
+
+        /// <summary>
+        /// Get a Local Storage item's value by the key identifier
+        /// </summary>
+        /// <param name="key">The key identifier of the local storage item</param>
+        /// <returns>The value of the Local Storage item</returns>
         public async ValueTask<string> GetItem(string key)
         {
             var module = await moduleTask.Value;
             return await module.InvokeAsync<string>("GetItem", key);
         }
 
+  
+
+
+        /// <summary>
+        /// Remove a Local Storage item by the key identifier
+        /// </summary>
+        /// <param name="key">The key identifier of the local storage item</param>
         public async ValueTask RemoveItem(string key)
         {
             var module = await moduleTask.Value;
             await module.InvokeVoidAsync("RemoveItem", key);
         }
 
+        /// <summary>
+        /// Clear all items in the Local Storage
+        /// </summary>
         public async ValueTask Clear()
         {
             var module = await moduleTask.Value;
